dd85de99af658c54f1a087b5ced7df16e6808e30
Itsho <itsho.itsho@gmail.com>
2017-05-17 12:01:22 +0300

    Initial commit

---
+++ PreviousDataBindingWheelIssue/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace PreviousDataBindingWheelIssue
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MyModel();
+        }
+    }
+}
---
+++ PreviousDataBindingWheelIssue/MyModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PreviousDataBindingWheelIssue
+{
+    public class MyModel
+    {
+        public ObservableCollection<string> MyData { get; set; }
+
+        public MyModel()
+        {
+            MyData = new ObservableCollection<string>()
+            {
+                "Lorem ipsum dolor", "sit amet, consectetur", "adipiscing elit. Sed",
+                "Fun Item",
+                "rhoncus leo convallis", "pulvinar tellus at",
+                "Fun Item",
+                "porta metus. Mauris", "sed mauris quis", "neque congue semper",
+                "Fun Item",
+                "vitae non leo", "Donec aliquet feugiat", "massa vitae luctus",
+                "Fun Item",
+                "Duis pharetra velit", "et lorem blandit"
+            };
+        }
+    }
+}
